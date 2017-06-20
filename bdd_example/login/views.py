f301e16eaec8e49956937462eb6369ce2579c490
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-06-20 17:51:49 -0500

    Add login capability (we do not remenber sessions)

--- bdd_example/login/views.py
+++ bdd_example/login/views.py
@@ -1,3 +1,24 @@
+from django.contrib.auth import authenticate
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+
+def login_root(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                return HttpResponseRedirect(reverse('login_success'))
+        return HttpResponseRedirect(reverse('login_fail'))
+    return render(request, 'login_root.html')
+
+
+def login_fail(request):
+    return render(request, 'login_fail.html')
+
+
+def login_success(request):
+    return render(request, 'login_success.html')
